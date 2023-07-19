ed2a8055f1b504b5f66363b65238bff44d7b02cd
Clément Jacquet <clem.jacquet@gmail.com>
2023-07-20 00:22:49 +0900

    fix: wrong format in screen quad vertex attribute

--- src/model.rs
+++ src/model.rs
@@ -29,7 +29,7 @@ impl ScreenVertex {
                 wgpu::VertexAttribute {
                     offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
                     shader_location: 1,
-                    format: wgpu::VertexFormat::Float32x3,
+                    format: wgpu::VertexFormat::Float32x2,
                 }
             ]
         }
