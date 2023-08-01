4d3ecf1e9bed1f6c2879598fafa2fc96a2577d39
Clément Jacquet <clem.jacquet@gmail.com>
2023-08-02 01:09:55 +0900

    ref: clean camera.rs

--- src/camera.rs
+++ src/camera.rs
@@ -1,13 +1,4 @@
 use cgmath::SquareMatrix;
-use cgmath::Zero;
-
-#[rustfmt::skip]
-pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
-    1.0, 0.0, 0.0, 0.0,
-    0.0, 1.0, 0.0, 0.0,
-    0.0, 0.0, 0.5, 0.0,
-    0.0, 0.0, 0.5, 1.0,
-);
 
 pub struct Camera {
     pub eye: cgmath::Point3<f32>,
